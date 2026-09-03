226d04a2135706321a19228642412a948b016eaf
Y <n.251198.09@gmail.com>
2026-09-03 14:39:37 +0530

    all set but not colors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2224683837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3252915691" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
